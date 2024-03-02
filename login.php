c7c49a1b5c00ecb93291c46951dd6830a61824cb
TechEternalJourney <naufal.abhirama8@gmail.com>
2024-03-02 10:24:29 +0700

    menambahkan section navbar

--- login.php
+++ login.php
@@ -6,7 +6,17 @@
 </head>
 <body>
     
-    
+    <section id="navbar">
+        <nav class="container-navbar">
+            <div class="container-nav">
+                <ul class="container-ul">
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>Event</li>
+                </ul>
+            </div>
+        </nav>
+    </section>
 
 </body>
 </html>
